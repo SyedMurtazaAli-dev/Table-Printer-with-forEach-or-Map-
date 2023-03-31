2725b71932f9daae7964e3cb97241e1727fe25e9
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-03-31 23:16:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+    var b = prompt("which table you want to print");
+    var a = prompt("give table length");
+    let arr = Array(parseInt(a)).fill().map( function(_ , i){
+        return i + 1;
+    } );
 
+    arr.forEach(function( number , index ){
+        document.write( b + " x " + number + " = " + b * number + "<br>" );
+        
+    })
